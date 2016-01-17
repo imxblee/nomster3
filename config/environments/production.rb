dc737e6275eb354e00b468b4d0880fc7c7612b46
imxblee <transcendencez@gmail.com>
2016-01-17 13:35:15 +0000

    setup devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Nomster3::Application.configure do
+   config.action_mailer.default_url_options = { host: 'nomster3-xb.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
