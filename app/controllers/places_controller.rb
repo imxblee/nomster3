031756d477edaa43139f678d4ce0603a0fe16652
imxblee <transcendencez@gmail.com>
2016-01-18 03:17:19 +0000

    improve places#edit and update action

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create,:edit,:update]
 	def index
 		@places = Place.page(params[:page]).per(3)
 	end
@@ -15,9 +15,16 @@ class PlacesController < ApplicationController
 	end
 	def edit
 		@place = Place.find(params[:id])
+
+		if @place.user != current_user
+			return render text: "Not Allowed", status: :forbidden
+		end
 	end
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
